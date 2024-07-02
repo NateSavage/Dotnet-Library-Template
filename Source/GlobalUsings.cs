2b618727453c5358e928627eb9747c5e289d565e
Nate Savage <Nate.Savage@TexelDrain.com>
2024-07-02 16:55:07 -0500

    Exposed Internal Members To Benchmark & Test Projects

--- Source/GlobalUsings.cs
+++ Source/GlobalUsings.cs
@@ -1,2 +1,5 @@
 global using System;
 global using System.Collections.Generic;
+
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Benchmarks")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Tests")]
